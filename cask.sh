571074ddeb0416eed6597e8ac014a8d3c70c58c6
Charles P. Oliver <charles.oliver.extern@holidaycheck.com>
2017-05-16 13:12:30 +0200

    Add script to install apps from brew cask

---
+++ cask.sh
@@ -0,0 +1,46 @@
+# Install cask packages.
+
+# Communications
+brew cask install screenhero
+brew cask install skype
+brew cask install slack
+brew cask install whatsapp
+
+# Development
+brew cask install atom
+brew cask install haskell-platform
+brew cask install iterm2
+brew cask install macdown
+brew cask install robomongo
+brew cask install spacemacs
+brew cask install sublime-text
+brew cask install visual-studio-code
+
+# Internetz
+brew cask install epichrome
+brew cask install google-chrome
+brew cask install transmission
+
+# Media
+brew cask install 4k-video-downloader
+brew cask install plex-media-player
+brew cask install plex-media-server
+brew cask install spotify
+brew cask install vlc
+
+# Music Production
+brew cask install splice
+
+# Utilities
+brew cask install 1password
+brew cask install alfred
+brew cask install appcleaner
+brew cask install bartender
+brew cask install bettertouchtool
+brew cask install box-sync
+brew cask install itsycal
+brew cask install jing
+brew cask install moom
+brew cask install namechanger
+brew cask install synergy
+brew cask install typinator

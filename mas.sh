aebd537d42e5e8a9a33217a8214d5acb6392f23a
Charles P. Oliver <charles.oliver.extern@holidaycheck.com>
2017-05-16 13:13:02 +0200

    Add script to install apps from the mac app store

---
+++ mas.sh
@@ -0,0 +1,9 @@
+# Install mac app store apps.
+
+# mas signin mas@example.com
+
+mas install 918858936 # Airmail 3
+mas install 824183456 # Affinity Photo
+mas install 824171161 # Affinity Designer
+mas install 425424353 # The Unarchiver
+mas install 585829637 # Todoist

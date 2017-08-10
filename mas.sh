a92749ab51b7e6f9fddc4792a1d3ccbaa70b09b8
Charles P Oliver <charles@cocoa.consulting>
2017-08-10 08:28:23 +0100

    Update Mac App Store apps

--- mas.sh
+++ mas.sh
@@ -2,9 +2,18 @@
 
 # mas signin mas@example.com
 
-mas install 918858936 # Airmail 3
-mas install 824183456 # Affinity Photo
-mas install 824171161 # Affinity Designer
-mas install 425424353 # The Unarchiver
-mas install 585829637 # Todoist
-mas install 497799835 # Xcode
+mas install 1096528668 #1Doc (lighweight Word replacement)
+mas install 824171161  #Affinity Designer
+mas install 824183456  #Affinity Photo
+mas install 918858936  #Airmail 3
+mas install 937984704  #Amphetamine
+mas install 581915465  #Colors (control Hue from menubar)
+mas install 971826370  #Extractor
+mas install 668208984  #GIPHY CAPTURE
+mas install 435932420  #iPackr (1.50)
+mas install 953006734  #JSON Query
+mas install 669475285  #PG Commander
+mas install 412843352  #QuickNFO
+mas install 970432740  #Right Click Booster
+mas install 585829637  #Todoist
+mas install 497799835  #Xcode
